7d70c6873eb868c5f20f959034c97d1b347443b4
Ubuntu <ubuntu@ip-172-31-44-181.us-west-2.compute.internal>
2015-07-06 13:06:25 -0600

    fix set loader getting called during build, keep usermodel in cache

--- loaders/set.js
+++ loaders/set.js
@@ -6,7 +6,7 @@ define(['node_modules/thumos/lib/setBuilder'], function(setBuilder){
 	return {
 		load : function(name, req, onload, config, isBuild){
 			req([name], function(mod){ //require the set definition
-				onload(isBuild?null:setBuilder(mod)); //if we're in a build don't call the definition since mod will be undefined
+				onload(!mod?null:setBuilder(mod)); //if we're in a build don't call the definition since mod will be undefined
 			})
 		}
 	}
